597212dcb6a2f04f33947d3f50b2accac6a61e08
Ganesh Bedake <ganeshbedake7285@gmail.com.com>
2026-03-24 10:28:44 +0530

    Fix import path for api module

--- src/pages/trainer/TrainerModules.jsx
+++ src/pages/trainer/TrainerModules.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import api from "../api";
+import api from '../../api';
 
 const TrainerModules = ({ courseId }) => {
   const [modules, setModules] = useState([]);
